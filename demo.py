d1d87dc01fc14b9d82d74a6ce69fd5809a6c990d
Sizhe Chen <44351170+Sizhe-Chen@users.noreply.github.com>
2026-02-04 14:03:19 -0800

    Remove copyright and license comments from demo.py
    Removed copyright notice and license information.

--- demo.py
+++ demo.py
@@ -1,8 +1,3 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-
 from vllm import LLM, SamplingParams
 from vllm.lora.request import LoRARequest
 from huggingface_hub import login
